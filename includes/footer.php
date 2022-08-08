ab0824879e921986f6985c78189aacb872ad16e9
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2022-08-08 14:36:23 -0300

    Erro ao carregar Slid

---
+++ includes/footer.php
@@ -0,0 +1,51 @@
+<footer class="container-fluid footer">
+    <div class="container">
+        <div class="row text-center text-lg-start">
+            <div class="col-lg col-12 my-5 my-lg-0">
+                <a href="/home.php"><img src="img/logo.png" alt="logo da empresa" width="200px"></a>
+                <p class="mt-3">
+                    Ciência e Tecnologia, S.A<br>
+                    87.580-000 Alto Piquiri-PR
+                </p>
+                <div>
+                    <i class="bi bi-facebook fs-3 me-3"></i>
+                    <i class="bi bi-instagram fs-3"></i>
+                </div>
+            </div>
+            <div class="col-6 col-lg offset-lg-2 d-flex flex-column gap-2 mb-4">
+                <h4>Website</h4>
+                <a href="#">Home</a>
+                <a href="#">Sobre Nós</a>
+                <a href="#">Loja</a>
+                <a href="#">Contato</a>
+                <a href="#">A minha conta</a>
+            </div>
+            <div class="col-6 col-lg offset-lg-2 d-flex flex-column gap-2 mb-4">
+                <h4>Website</h4>
+                <a href="#">Home</a>
+                <a href="#">Sobre Nós</a>
+                <a href="#">Loja</a>
+                <a href="#">Contato</a>
+                <a href="#">A minha conta</a>
+            </div>
+            <div class="col-6 col-lg offset-lg-2 d-flex flex-column gap-2 mb-4">
+                <h4>Website</h4>
+                <a href="#">Home</a>
+                <a href="#">Sobre Nós</a>
+                <a href="#">Loja</a>
+                <a href="#">Contato</a>
+                <a href="#">A minha conta</a>
+            </div>
+        </div>
+        <div>
+            <hr>
+            <h5>© <?php echo date("Y"); ?> - Julio Paschoal
+            </h5>
+        </div>
+    </div>
+</footer>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php require_once('includes/header.php'); ?>
+
+<!-- Caousel -->
+<section>
+    <div id="carouselExampleCaptions" class="carousel slide carousel-home" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item carousel-image active">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <h2 class="mb-4">Tecnologia acessível<br> num só sítio</h5>
+                            <a href="loja.php" class="btn-carousel">Ver mais</a>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item carousel-image">
+                <div class="carousel-caption ">
+                    <div class="container">
+                        <h2 class="mb-4">Tecnologia acessível<br> num só sítio</h5>
+                            <a href="loja.php" class="btn-carousel">Ver mais</a>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item carousel-image">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <h2 class="mb-4">Tecnologia acessível<br> num só sítio</h5>
+                            <a href="loja.php" class="btn-carousel">Ver mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <!-- Novidades -->
+    <section class="novidades">
+        
+    </section>
+
+<?php require_once('includes/footer.php'); ?>
